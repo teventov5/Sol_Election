1bae039a356d0b794c1c7ebc8982e383557514f8
teventov5 <teventov@gmail.com>
2022-01-30 14:59:45 +0200

    1. initialization

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+    // Read/write candidate
+    string public candidate;
+
+    // Constructor
+    constructor () public {
+
+        candidate="candidate 1";
+    }
+}
