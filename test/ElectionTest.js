2d49c905b94ea1557e768b2abd9cf3e7e52d0085
teventov5 <teventov@gmail.com>
2022-02-01 15:15:59 +0200

    completed all tests.

--- test/ElectionTest.js
+++ test/ElectionTest.js
@@ -1,12 +1,38 @@
-var ElectionTest=artifacts.require("./Election.sol");
+var ElectionContract=artifacts.require("./Election.sol");
 
 contract ("Election",function(accounts){
 
-  it("Init with only two nominees",function(){
-    return ElectionTest.deployed().then(function(instance){
+  it("assertion there are only two nominees",function(){
+    return ElectionContract.deployed().then(function(instance){
       return instance.totalNomineesCount();
     }).then(function(count){
       assert.equal(count,2);
     });
   });
+
+
+  it("assertion nominee has correct values", function(){
+    return ElectionContract.deployed().then(function(instance){
+      electionInstance=instance;
+      return electionInstance.totalNominees(1);
+    }).then(function(nominee1){
+      assert.equal(nominee1[0],1, "has the correct ID");
+      assert.equal(nominee1[1],0, "has the correct voteCount");
+      assert.equal(nominee1[2],"Tom", "has the correct name");
+
+      return electionInstance.totalNominees(2);
+    }).then(function(nominee2){
+      assert.equal(nominee2[0],2, "has the correct ID");
+      assert.equal(nominee2[1],0, "has the correct voteCount");
+      assert.equal(nominee2[2],"Mishael", "has the correct name");
+
+    });
+  });
+
+
+
+
+
+
+
 });
